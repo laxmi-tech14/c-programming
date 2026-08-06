c053e6be81197708e7b4cd05165986aba4a2eead
Vennam Laxmi <vennamlaxmivennam@gmail.com>
2026-08-06 17:56:05 +0530

    Add files via upload

---
+++ 50_array_of_numbers.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int marks [4]={45,78,90,34};
+   for(int i=0;i<4;i++)
+   {
+       printf(" the value of %d element of the array is %d\n",i,marks[i]);
+   }
+ 
+    return 0;
+}
